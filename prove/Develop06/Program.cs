2bd4041ba87a02148d85c4c6ef6d51c1547bea1d
tjlowr <tjlowry02@gmail.com>
2024-10-26 19:08:05 -0600

    eternal quest strecch comment

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,5 +1,6 @@
 using System;
-
+// I added the ability to see how many times each goal type has been recoreded along with how many times each eternal goal has been completed
+// I also added ASCII fireworks for when you comeplete a goal
 class Program
 {
     static void Main(string[] args)
